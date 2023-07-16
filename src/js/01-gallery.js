643d33ec28ead34c2882d3cdbf13fa53b3e9f791
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-07-16 22:01:32 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const containerGalleryItems = document.querySelector('.gallery');
+
+const createGalleryMarkyp = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+containerGalleryItems.insertAdjacentHTML('beforeend', createGalleryMarkyp);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
