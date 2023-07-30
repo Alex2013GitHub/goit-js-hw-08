2e3363c5dd43efe2cf994211d3105d041ee8b437
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-07-30 22:27:17 +0200

    home_work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+handleLocalStorage();
+
+refs.form.addEventListener('submit', handleSubmit);
+refs.input.addEventListener('input', throttle(handleInput, 500));
+
+function handleInput() {
+  const formData = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+  refs.form.reset();
+}
+
+function handleLocalStorage() {
+  const defaultvalue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!defaultvalue) {
+    return;
+  }
+  refs.input.value = defaultvalue.email || '';
+  refs.textarea.value = defaultvalue.message || '';
+}
